64dd824fb11589928695774295da0922bff54fbd
opre0502 <140704747+opre0502@users.noreply.github.com>
2023-11-29 19:16:55 +0900

    Add files via upload

---
+++ was-practice/src/main/java/org/example/calculate/DivisionOperator.java
@@ -0,0 +1,17 @@
+package org.example.calculate;
+
+public class DivisionOperator implements NewArithmeticOperator {
+
+    @Override
+    public boolean supports(String operator) {
+        return "/".equals(operator);
+    }
+
+    @Override
+    public int calculate(PositiveNumber operand1, PositiveNumber operand2) {
+//        if(operand2.toInt() == 0) {
+//            throw new IllegalArgumentException("나눗셈에서 0으로 나눌수 없습니다.");
+//        }
+        return operand1.toInt() / operand2.toInt();
+    }
+}
